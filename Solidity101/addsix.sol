8edf0502f95ba5a610e7df49937a6a95c05784ba
Hcrlen <93064083+ykgneh@users.noreply.github.com>
2025-04-30 23:25:55 +0700

    Update addsix.sol

--- Solidity101/addsix.sol
+++ Solidity101/addsix.sol
@@ -4,7 +4,7 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract AddSix is SimpleStorage {
 
-    //same function name + overide (child)
+    //same function name + override (child)
     //same function name + virtual (parent)
     function store(uint256 _added) public override  {
       FavoriteNumber = _added + 6;
